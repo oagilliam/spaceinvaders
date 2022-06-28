6905f71f07a1db82049f6f002f5cf448718391be
oagilliam <75933763+oagilliam@users.noreply.github.com>
2022-06-28 11:09:00 -0400

    main

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ screen = pygame.display.set_mode((800, 600))
 
 # Title and Icon
 pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load("Spaceship1.png")
+pygame.display.set_icon(icon)
 
 # Game Loop
 running = True
